2b4e2bbd6d1f6ea8851bd88608ba4cfcab4255b7
Maxime Santarelli <174466723+Xk04@users.noreply.github.com>
2026-02-23 00:13:32 +0100

    fonts, background, + project cards

--- public/js/project_list.js
+++ public/js/project_list.js
@@ -27,7 +27,7 @@ let personalProjects = [
   // Tank
   new Preview(
     "public/images/preview/tank.png",
-    "Autonomous Robot — Yahboom Tank",
+    "Autonomous Robot v.2",
     "",
     "01/2026",
     "Fabrication Lab",
@@ -36,7 +36,6 @@ let personalProjects = [
     "Autonomous Robot — Yahboom Tank",
     `
       <p>
-      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
       </p>
     `,
     `
@@ -50,7 +49,7 @@ let personalProjects = [
   // Tiny Bot
   new Preview(
     "public/videos/preview/tiny_bot.gif",
-    "Autonomous Robot — Yahboom Tiny Bot",
+    "Autonomous Robot v.1",
     "Able to make choices based on signage",
     "06/2025",
     "Fabrication Lab",
@@ -58,11 +57,17 @@ let personalProjects = [
   new Project(
     "Autonomous Robot — Yahboom Tiny Bot",
     `
+    <iframe src="https://www.youtube.com/embed/4n_6p8l14WA" title="Yahboom Tiny Bot - Test phase" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
     <p>
-    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        My first project at the Fabrication Laboratory (with two teammates mentionned in the slides) was a Tiny bot from the Yahboom company.<br>
+        This autonomous robot was pretty good to understand minimal IoTs' architecture.<br>
+        We also discovered the <a href="https://docs.ultralytics.com/" target="_blank" rel="noopener">You Only Look Once</a> CNN model from ultralytics.<br>
+        As you can see, this robot is really tiny. Then I decided to continue with a v.2 the "Yahboom G1 AI vision - Tank".
     </p>
     `,
-    "",
+    `
+      <a href="https://www.canva.com/design/DAGojOKnQJc/a5PDoHsTN3Pe8AiHZA6tEQ/view?utm_content=DAGojOKnQJc&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h7383905023" target="_blank" rel="noopener">Explanations in few slides (french version)</a>
+    `,
   ),
 ];
 
@@ -72,12 +77,12 @@ let collegeProjects = [
   // Compiler
   new Preview(
     "public/images/preview/mycompiler.png",
-    "Compiler - MyCompiler",
+    "Creating a Compiler",
     `
       Project in progress...
     `,
     "~ /2026",
-    "Compilation",
+    "Compilation course",
   ),
   new Project(
     "Creating a Compiler - MyCompiler",
@@ -108,7 +113,11 @@ let collegeProjects = [
     "Game Engine in Java",
     `
       <p>
-      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+          During one semester we create a simple 2D Game Engine for plateformer games.<br>
+          We didn't have to build a video game, we needed to create the simplest possible structure in order to welcome any projects.<br>
+          Then I did the software design with UML, the structure, the window creation/management, models, and many controllers, (+ docs).<br>
+          This Game Engine helps us to practice what we saw in Object Oriented Programming and Design.<br>
+          It improved drastically my design software thinking. Nowadays I can organize more properly my new projects.
       </p>
     `,
     `
@@ -122,7 +131,7 @@ let collegeProjects = [
   // Sport betting website
   new Preview(
     "public/videos/preview/lose1max.gif",
-    "Sport Betting website — Lose1Max",
+    "Sport Betting website",
     "",
     "06/2025",
     "Web dev course",
@@ -140,7 +149,11 @@ let collegeProjects = [
             allowfullscreen>
         </iframe>
         <p>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        During one semester we learned how to build a website in PHP, MySql (+ Ajax system).<br>
+        Then we understood backend, security mecanisms, database and frontend in order to create a sport betting app.<br>
+        I implemented the backend, security methods, an admin panel and many frontend pages.<br>
+        For more details you can watch the youtube video in which my teammate present the website in the 1st part.
+        And I talk about my work in the 2nd part. You can also visit the public repository on GitHub !<br>
         </p>
     `,
 
@@ -149,13 +162,17 @@ let collegeProjects = [
       <img src="public/images/icons/github.png" alt="">
       <p>For more details, go to the repository here</p>
     </a>
+    <p>
+      This project allows me to think about scalablity, security, and data management.<br>
+      I have learned a lot of mecanisms at this moment. I'm glad to have discovered the different processes involved in building a website.
+    </p>
     `,
   ),
 
   // Polynomial manipulation
   new Preview(
     "public/images/preview/R.png",
-    "Polynomial Manipulation in R",
+    "Polynomial Manipulation",
     `
             Polynomial algorithms & tests automation
         `,
